ea9eecd5f22d19f4d0cb8f4f80d041af6b91f8bd
byron <a.c.m.7.1@hotmail.com>
2016-09-19 10:36:10 -0500

    Update Contents.swift

--- Contents.swift
+++ Contents.swift
@@ -7,26 +7,19 @@ var temp: Int = 0
 for i in rango{
     
     if i%5 == 0{
-        temp = 1
-    }else if i>=30 && i<=40{
-        temp = 2
-    }else if i%2 == 0{
-        temp = 3
-    }else if i%2 != 0{
-        temp = 4
+        print(" \(i) Bingo!!!")
     }
     
-    switch temp {
-    case 1:
-        print(" \(i) Bingo!!!")
-    case 2:
+    if i>=30 && i<=40{
         print(" \(i) Viva Swift!!!")
-    case 3:
-        print(" \(i) par!!!")
-    case 4:
-        print(" \(i) impar!!!")
-    default:
-        print("")
     }
-    temp = 0
+    
+    if i%2 == 0{
+        print(" \(i) par!!!")
+    }
+    
+    if i%2 != 0{
+        print(" \(i) impar!!!")
+    }
+
 }
